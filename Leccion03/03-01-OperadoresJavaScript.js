e5503cc0042f967cd678af44bc31cc2533556b02
AniC <111471233+anacastello19@users.noreply.github.com>
2022-11-14 14:01:20 -0300

    Leccion3
    Ejercicios Funcion isNaN, operador ternario entre otros

--- Leccion03/03-01-OperadoresJavaScript.js
+++ Leccion03/03-01-OperadoresJavaScript.js
@@ -26,3 +26,42 @@ if (dantroRango>= valMin && dantroRango<=valMax){
 else{
     console.log('Esta fuera del rango establecido');
 }
+//Ejercicio: Si el padre puede asistir al juego de su hija
+let vacaciones = true, diaDeDescanso = false;
+if (vacaciones||diaDeDescanso){
+    console.log("El padre puede asistir al juego de su hijo")
+
+}
+else{
+    console.log("El padre NO puede asistor al jueg de su hijo")
+}
+//Operador ternario
+let rdos2 =6>5? "Verdadero":"Falso";
+console.log(rdos2)
+let num= 8;
+rdos2= num%2==0? "Verdadero":"Falso";
+console.log(rdos2)
+//Convertir String a Number
+let miNum="18";
+console.log(typeof miNum);
+let edad2= Number(miNum);
+console.log(typeof edad2);
+
+//Funcion isNaN
+if(isNaN(edad2)){//No es un numero, devuelve un resultado booleano
+    console.log("Esta variable no contuene solo numeros")
+}
+else{
+    if(edad2>=18){
+        console.log("Puede votar");
+    }
+    else{
+        console.log("Es muy joven para votar")
+    }
+}
+
+
+let rdo3= edad2>=18? "Puede votar" : "Es muy joven para votar";
+console.log(rdo3)
+
+
